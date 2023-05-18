76c1c644271797acfecd40b6184a76cb65987873
anggiazizah <anggiazizah18@gmail.com>
2023-05-18 13:37:33 +0700

    buat index php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Biodata Kelas</title>
+    <link rel="stylesheet"href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="title">Form Biodata Kelas</div>
+        <div class="content">
+            <From action="#">
+                <div class="user-details">
+                    <div class="input-inbox">
+                        <span class= "details">Nama Lengkap</span>
+                        <span class= "details">:</span>
+                        <input type="text" placeholder="Masukan nama lengkap" required>
+                    </div>
+                   <tr>
+                    <td>Kelas</td>
+                    <td>:</td>
+                    <td><select name="" id="">
+                        <option value="">11 RPL 1</option>
+                        <option value="">11 RPL 2</option>
+                        </select>
+                    </td>
+                   </tr>
+                   <div class="user-details">
+                    <div class="input-inbox">
+                        <span class= "details">Alamat</span>
+                        <span class= "details">:</span>
+                        <textarea name="" id="" cols="30" rows="10"></textarea>
+                        <!-- <input type="text" placeholder="Masukan alamat" required> -->
+                    </div>
+                   <!-- <br>
+                   <tr>
+                    <td>Alamat</td>
+                    <td><textarea name="" id="" cols="30" rows="10"></textarea></td>
+                </tr> -->
+                </div>
+            </From>
+        </div>
+    </div>
+    
+</body>
+</html>
